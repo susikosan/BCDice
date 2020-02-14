61044e2219452410490d7416b23df08109bf94d5
SAKATA Sinji <ysakasin@gmail.com>
2020-02-15 01:17:57 +0900

    Refactor D66 and add testcases

--- src/dice/d66_dice.rb
+++ src/dice/d66_dice.rb
@@ -4,93 +4,33 @@ module D66Dice
       return nil
     end
 
-    unless /^S?D66/i.match(string)
-      return nil
-    end
-
-    debug("match D66 roll")
-    output, secret = d66dice(string)
-
-    @secret = secret
-    return output
-  end
-
-  def d66dice(string)
-    string = string.upcase
-    secret = false
-    output = '1'
-
-    string, secret, count, swapMarker = getD66Infos(string)
-    return output, secret if string.nil?
-
-    debug('d66dice count', count)
-
-    d66List = []
-    count.times do |_i|
-      d66List << getD66ValueByMarker(swapMarker)
-    end
-    d66Text = d66List.join(',')
-    debug('d66Text', d66Text)
-
-    output = ": (#{string}) ＞ #{d66Text}"
-
-    return output, secret
-  end
-
-  def getD66Infos(string)
-    debug("getD66Infos, string", string)
-
-    m = /(^|\s)(S)?((\d+)?D66(N|S)?)(\s|$)/i.match(string)
+    m = /^(S)?((\d+)?D66([NS])?)$/i.match(string)
     unless m
       return nil
     end
 
-    secret = !m[2].nil?
-    string = m[3]
-    count = (m[4] || 1).to_i
-    swapMarker = (m[5] || "").upcase
+    @secret = !m[1].nil?
+    command = m[2]
+    times = m[3]&.to_i || 1
+    suffix = m[4]
 
-    return string, secret, count, swapMarker
+    swap_type = swap_type_by_suffix(suffix)
+
+    dice_values = @randomizer.roll_d66(times, swap_type)
+    return ": (#{command}) ＞ #{dice_values.join(',')}"
   end
 
-  def getD66ValueByMarker(swapMarker)
-    case swapMarker
-    when "S"
-      isSwap = true
-      rollD66(isSwap)
-    when "N"
-      isSwap = false
-      rollD66(isSwap)
+  # @param [String]
+  # @return [Symbol]
+  def swap_type_by_suffix(suffix)
+    if suffix == "S"
+      :asc
+    elsif suffix == "N"
+      :none
+    elsif @d66Type > 1
+      :asc
     else
-      getD66Value()
+      :none
     end
-  end
-
-  def getD66Value(mode = nil)
-    mode ||= @d66Type
-
-    isSwap = (mode > 1)
-    rollD66(isSwap)
-  end
-
-  def rollD66(isSwap)
-    output = 0
-
-    dice_a = @randomizer.rand(6)
-    dice_b = @randomizer.rand(6)
-    debug("dice_a", dice_a)
-    debug("dice_b", dice_b)
-
-    if isSwap && (dice_a > dice_b)
-      # 大小でスワップするタイプ
-      output = dice_a + dice_b * 10
-    else
-      # 出目そのまま
-      output = dice_a * 10 + dice_b
-    end
-
-    debug("output", output)
-
-    return output
   end
 end
